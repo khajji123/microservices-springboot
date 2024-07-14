90375d29507770527244a05e7be6e8570bfafbd3
jerbi2026 <jerbiahmed24@gmail.com>
2024-07-14 13:31:08 +0100

    projet microservices

--- site-service/src/main/java/com/khalil/siteservice/SiteServiceApplication.java
+++ site-service/src/main/java/com/khalil/siteservice/SiteServiceApplication.java
@@ -3,6 +3,7 @@ package com.khalil.siteservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class SiteServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
